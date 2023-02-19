0df0ddc1fbc0dbc9087a40c6d5f3b65c3acb7127
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2023-02-19 17:45:49 +0900

    [add] トップのログインページを実装

---
+++ src/pages/notes.tsx
@@ -0,0 +1,24 @@
+import { NextPage } from 'next';
+import {
+  ArrowLeftOnRectangleIcon,
+  DocumentTextIcon,
+  BellIcon,
+} from '@heroicons/react/24/solid';
+import { supabase } from '@/utils/supabase';
+import { Layout } from '@/components/Layout';
+
+const Notes: NextPage = () => {
+  const signOut = () => {
+    supabase.auth.signOut();
+  };
+  return (
+    <Layout title="Notes">
+      <ArrowLeftOnRectangleIcon
+        className="mb-6 h-6 w-6 cursor-pointer text-blue-500"
+        onClick={signOut}
+      />
+    </Layout>
+  );
+};
+
+export default Notes;
